4d398f6ad3ae9d492b3bb7fa5b8c17112572c507
nasimhf <mohmadabrhami34@gmail.com>
2026-03-07 07:14:25 +0100

    Add files via upload

---
+++ tool.py
@@ -0,0 +1,46 @@
+#!/usr/bin/env python3
+
+import requests
+import os
+import sys
+
+VERSION = "1.0"
+
+VERSION_URL = "https://raw.githubusercontent.com/username/project/main/version.txt"
+SCRIPT_URL = "https://raw.githubusercontent.com/username/project/main/tool.py"
+
+
+def check_update():
+    try:
+        print("🔍 فحص التحديثات...")
+
+        r = requests.get(VERSION_URL, timeout=10)
+        latest_version = r.text.strip()
+
+        if latest_version != VERSION:
+            print("🔄 يوجد تحديث جديد:", latest_version)
+
+            code = requests.get(SCRIPT_URL).text
+
+            with open(os.path.basename(__file__), "w", encoding="utf-8") as f:
+                f.write(code)
+
+            print("✅ تم تحديث الأداة")
+            print("♻️ أعد تشغيل البرنامج")
+            sys.exit()
+
+        else:
+            print("✅ لديك أحدث إصدار")
+
+    except:
+        print("⚠️ تعذر التحقق من التحديث")
+
+
+check_update()
+
+
+print("🚀 مرحبا بك في الأداة")
+
+name = input("ادخل اسمك: ")
+
+print("اهلا", name)
