7cc3a5b78770b28b5c0c1c832898b85e9ca43eac
nasimhf <mohmadabrhami34@gmail.com>
2026-03-07 09:06:01 +0100

    Update tool.py

--- tool.py
+++ tool.py
@@ -6,7 +6,7 @@ import os
 import sys
 import time
 #hhhhjj
-VERSION = "1.1"
+VERSION = "1.2"
 VERSION_URL = "https://raw.githubusercontent.com/nasimhf/monir/main/version.txt"
 SCRIPT_URL = "https://raw.githubusercontent.com/nasimhf/monir/main/tool.py"
 
@@ -57,3 +57,32 @@ def main_tool():
 if __name__ == "__main__":
     check_update()
     main_tool()
+    import requests
+import sys
+import time
+
+CHECK_URL = "https://raw.githubusercontent.com/nasimhf/Ahmad/main/karim"
+
+def check_subscription():
+    try:
+        r = requests.get(CHECK_URL, timeout=10)
+        data = r.text.strip()
+
+        if "amin" in data:
+            print("✅ الاشتراك مفعل")
+            return True
+        else:
+            print("❌ انتهى الاشتراك")
+            return False
+
+    except Exception as e:
+        print("⚠️ تعذر الاتصال بالخادم")
+        return False
+
+
+if not check_subscription():
+    print("🔒 عليك تجديد الاشتراك")
+    sys.exit()
+
+print("🚀 مرحبا تم الاشتراك")
+input("اضغط Enter للمتابعة...")
