180bf4ff06c14d4589d7a8a2ab98c4445688ebbb
nasimhf <mohmadabrhami34@gmail.com>
2026-03-07 15:51:21 +0100

    Update tool.py

--- tool.py
+++ tool.py
@@ -5,10 +5,10 @@ import requests
 import os
 import sys
 import time
-#amin
+#ghg
 #fre fier
 # ================== إعدادات النسخة ==================
-VERSION = "1.2"
+VERSION = "1.1"
 VERSION_URL = "https://raw.githubusercontent.com/nasimhf/monir/main/version.txt"
 SCRIPT_URL = "https://raw.githubusercontent.com/nasimhf/monir/main/tool.py"
 
