3aa59c89416450b7321d02ac413ff0c5c59bcbbd
nasimhf <mohmadabrhami34@gmail.com>
2026-03-12 00:24:07 +0100

    Update version number from 1.2 to 1.3

--- tool.py
+++ tool.py
@@ -7,9 +7,10 @@ import sys
 import time
 #hhhhh
 #ghg
+#@
 #fre fier
 # ================== إعدادات النسخة ==================
-VERSION = "1.2"
+VERSION = "1.3"
 VERSION_URL = "https://raw.githubusercontent.com/nasimhf/monir/main/version.txt"
 SCRIPT_URL = "https://raw.githubusercontent.com/nasimhf/monir/main/tool.py"
 
